74f74855430efeb3fbd7d923b9202a459e5d7652
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-02-15 02:50:07 -0600

    Add module progress

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,13 @@
 const fs = require('fs')
 const player = require('../lib')
 
+function render(mod) {
+  process.stdout.moveCursor(0,-1)
+  console.log(mod.position);
+  mod.read(2048)
+  setTimeout(render.bind(null, mod), 10);
+}
+
 fs.readFile(process.argv[2], function(err, data) {
   if(err != null) {
       console.log(err);
@@ -9,6 +16,13 @@ fs.readFile(process.argv[2], function(err, data) {
           channels: 2,      // 2 channels (stereo)
           sampleRate: 48000 // 48,000 Hz sample rate
       });
-      console.log(mod.metadata)
+      console.log(mod.metadata.title)
+      console.log('Press any key to exit\n');
+
+      process.stdin.setRawMode(true);
+      process.stdin.resume();
+      process.stdin.on('data', process.exit.bind(process, 0));
+
+      render(mod);
   }
 });
